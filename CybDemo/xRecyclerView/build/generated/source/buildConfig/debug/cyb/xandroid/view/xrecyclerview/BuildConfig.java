9485ec4d99d266dfe683689a6b083c52077d27e1
LorensCyb <2981536120@qq.com>
2019-09-24 16:18:43 +0800

    【修改】 修改引用的包

--- CybDemo/xRecyclerView/build/generated/source/buildConfig/debug/cyb/xandroid/view/xrecyclerview/BuildConfig.java
+++ CybDemo/xRecyclerView/build/generated/source/buildConfig/debug/cyb/xandroid/view/xrecyclerview/BuildConfig.java
@@ -5,6 +5,11 @@ package cyb.xandroid.view.xrecyclerview;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "cyb.xandroid.view.xrecyclerview";
+  /**
+   * @deprecated APPLICATION_ID is misleading in libraries. For the library package name use LIBRARY_PACKAGE_NAME
+   */
+  @Deprecated
   public static final String APPLICATION_ID = "cyb.xandroid.view.xrecyclerview";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
